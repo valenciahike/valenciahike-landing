06091c37e14876e6ab04d484121fc8aa0121c160
Tirso Lecointere <tirsolecointere@gmail.com>
2023-06-27 17:37:35 -0400

    toggle menu on mobile

--- src/components/navComponent.tsx
+++ src/components/navComponent.tsx
@@ -1,8 +1,15 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link'
+import { useState } from 'react';
 
 export default () => {
     const router = useRouter();
+
+    const [clicked, setClicked] = useState(false);
+
+    const toggleMenu = () => {
+        setClicked(!clicked);
+    }
 
     return (
         <nav className="py-2">
@@ -12,17 +19,19 @@ export default () => {
                         <img src={`${router.basePath}/assets/images/logo_original.png`} className='w-24' alt="Valencia Hike" />
                     </Link>
                     <div className="lg:hidden flex items-center flex-nowrap gap-2 ml-auto">
-                        <button type="button" className="flex justify-center items-center bg-cream-yellow w-10 h-10 rounded-full text-center p-1">
+                        <button type="button" className="flex justify-center items-center bg-cream-yellow w-10 h-10 rounded-full text-center p-1"
+                            onClick={toggleMenu}>
                             <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" /> </svg>
                         </button>
                     </div>
                 </div>
-                <div id="menu-collapse" className="basis-full lg:basis-auto lg:ml-auto lg:!block" style={{ display: 'none' }}>
+                <div id="menu-collapse" className={`basis-full lg:basis-auto lg:ml-auto lg:!block ${clicked ? '!block' : ''}`} style={{ display: 'none' }}>
                     <ul className="flex items-center flex-col lg:flex-row gap-1">
                         <li> <Link href="/" className={`block hover:text-primary-light text-stone-800 uppercase font-title rounded-full px-4 py-2 text-sm ${router.pathname == '/' ? '!bg-primary !text-white' : ''}`}>Home</Link> </li>
                         <li> <Link href="/about" className={`block hover:text-primary-light text-stone-800 uppercase font-title rounded-full px-4 py-2 text-sm ${router.pathname == '/about' ? '!bg-primary !text-white' : ''}`}>Nosotros</Link> </li>
                         <li> <Link href="/faqs" className={`block hover:text-primary-light text-stone-800 uppercase font-title rounded-full px-4 py-2 text-sm ${router.pathname == '/faqs' ? '!bg-primary !text-white' : ''}`}>FAQs</Link> </li>
                         <li> <Link href="/tienda" className={`block hover:text-primary-light text-stone-800 uppercase font-title rounded-full px-4 py-2 text-sm ${router.pathname == '/tienda' ? '!bg-primary !text-white' : ''}`}>Tienda</Link> </li>
+                        <li> <Link href="/contact" className={`block hover:text-primary-light text-stone-800 uppercase font-title rounded-full px-4 py-2 text-sm ${router.pathname == '/contact' ? '!bg-primary !text-white' : ''}`}>Contacto</Link> </li>
                     </ul>
                 </div>
             </div>
