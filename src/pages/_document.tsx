bfa29d114baa0142fefdd8cefa483e42ac9f58d4
Tirso Lecointere <tirsolecointere@gmail.com>
2023-06-27 17:52:42 -0400

    fix crossorigin

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,7 @@ class MyDocument extends Document {
       <Html lang={AppConfig.locale}>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&family=Candal&family=Permanent+Marker&display=swap" rel="stylesheet" />
         </Head>
         <body className="text-stone-900">
