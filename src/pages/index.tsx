45f6aa159eb000c3aa116a8dea3c130dd6a6b106
Tirso Lecointere <tirsolecointere@gmail.com>
2023-07-06 12:54:35 -0400

    update team pictures and add social networks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -216,102 +216,142 @@ const Index = () => {
 
             <div className="container">
                 <div className="text-center max-w-3xl mx-auto mb-12">
-                    <h1 className="text-3xl lg:text-5xl font-title font-bold mb-4"><span className="text-primary-light">Conoce</span> Nuestro Equipo</h1>
+                    <h1 className="text-3xl lg:text-5xl font-title font-bold mb-4">Conoce <span className="text-primary-light">Nuestro Equipo</span></h1>
                     <div className="font-serif text-stone-500 mt-6">
                         <p className="md:text-lg mb-4">Nuestro equipo está conformado por 12 montañistas para las distintas tareas que se pueden manejar en una actividad. Entre ellos tenemos guías turísticos certificados, médicos, rescatista, médico veterinario, psicólogo y personal de apoyo con experiencia en senderismo de montaña.</p>
                     </div>
                 </div>
 
                 <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-3 xl:gap-8">
-                    <div className="group bg-white rounded-2xl text-center p-2 md:p-4">
-                        <div className="mb-4">
-                            <img src={`${router.basePath}/assets/images/team/01.png`} alt="" className="aspect-square object-cover rounded-lg" />
-                        </div>
-                        <div className="font-title mb-1 lg:mb-3 !leading-tight text-sm xl:text-base">Fernando Gómez</div>
-                        <div className="text-stone-400 font-serif text-sm leading-tight">Coordinador / Guía Turístico</div>
-                    </div>
-                    <div className="group bg-white rounded-2xl text-center p-2 md:p-4">
-                        <div className="mb-4">
-                            <img src={`${router.basePath}/assets/images/team/06.png`} alt="" className="aspect-square object-cover rounded-lg" />
-                        </div>
-                        <div className="font-title mb-1 lg:mb-3 !leading-tight text-sm xl:text-base">José Santana</div>
-                        <div className="text-stone-400 font-serif text-sm leading-tight">Médico</div>
-                    </div>
-                    <div className="group bg-white rounded-2xl text-center p-2 md:p-4">
-                        <div className="mb-4">
-                            <img src={`${router.basePath}/assets/images/team/05.png`} alt="" className="aspect-square object-cover rounded-lg" />
-                        </div>
-                        <div className="font-title mb-1 lg:mb-3 !leading-tight text-sm xl:text-base">Alejandra Burgos</div>
-                        <div className="text-stone-400 font-serif text-sm leading-tight">Médico</div>
-                    </div>
-                    <div className="group bg-white rounded-2xl text-center p-2 md:p-4">
-                        <div className="mb-4">
-                            <img src={`${router.basePath}/assets/images/team/04.png`} alt="" className="aspect-square object-cover rounded-lg" />
-                        </div>
-                        <div className="font-title mb-1 lg:mb-3 !leading-tight text-sm xl:text-base">Carlos Torrealba</div>
-                        <div className="text-stone-400 font-serif text-sm leading-tight">Médico Veterinario</div>
-                    </div>
-                    <div className="group bg-white rounded-2xl text-center p-2 md:p-4">
-                        <div className="mb-4">
-                            <img src={`${router.basePath}/assets/images/team/07.png`} alt="" className="aspect-square object-cover rounded-lg" />
-                        </div>
-                        <div className="font-title mb-1 lg:mb-3 !leading-tight text-sm xl:text-base">Ling Hung</div>
-                        <div className="text-stone-400 font-serif text-sm leading-tight">Psicólogo</div>
-                    </div>
-                    <div className="group bg-white rounded-2xl text-center p-2 md:p-4">
-                        <div className="mb-4">
-                            <img src={`${router.basePath}/assets/images/team/01.png`} alt="" className="aspect-square object-cover rounded-lg" />
-                        </div>
-                        <div className="font-title mb-1 lg:mb-3 !leading-tight text-sm xl:text-base">Juan Miguel Paolini</div>
-                        <div className="text-stone-400 font-serif text-sm leading-tight">Rescatista</div>
-                    </div>
-                    <div className="group bg-white rounded-2xl text-center p-2 md:p-4">
-                        <div className="mb-4">
-                            <img src={`${router.basePath}/assets/images/team/08.png`} alt="" className="aspect-square object-cover rounded-lg" />
-                        </div>
-                        <div className="font-title mb-1 lg:mb-3 !leading-tight text-sm xl:text-base">"Juancho" Nuñez</div>
-                        <div className="text-stone-400 font-serif text-sm leading-tight">Montañista / Trail Runner</div>
-                    </div>
-                    <div className="group bg-white rounded-2xl text-center p-2 md:p-4">
-                        <div className="mb-4">
-                            <img src={`${router.basePath}/assets/images/team/02.png`} alt="" className="aspect-square object-cover rounded-lg" />
-                        </div>
-                        <div className="font-title mb-1 lg:mb-3 !leading-tight text-sm xl:text-base">Andrea Rodriguez</div>
-                        <div className="text-stone-400 font-serif text-sm leading-tight">Colaborador</div>
-                    </div>
+                    {team.map(person => (
+                        <div className="group bg-white rounded-2xl text-center p-2 md:p-4">
+                            <div className="relative mb-4">
+                                <img src={`${router.basePath}/assets/images/team/${person.photo}`} alt="" className="aspect-square object-cover rounded-lg" />
 
-                    <div className="group bg-white rounded-2xl text-center p-2 md:p-4">
-                        <div className="mb-4">
-                            <img src={`${router.basePath}/assets/images/team/03.png`} alt="" className="aspect-square object-cover rounded-lg" />
+                                {/* rrss */}
+                                {person.rrss ?
+                                    <ul className='absolute left-0 -bottom-2 w-full flex items-center justify-center px-1 gap-1'>
+                                        {person.rrss.instagram ?
+                                            <li>
+                                                <a href={`https://www.instagram.com/${person.rrss.instagram}`} className="inline-flex items-center justify-center rounded-full text-xs h-6 w-6 bg-primary-light text-white shadow-lg shadow-primary/25" title="Ver Instagram" target="_blank">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="bi bi-instagram" viewBox="0 0 16 16"> <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/> </svg>
+                                                </a>
+                                            </li>
+                                            : null
+                                        }
+                                        {person.rrss.twitter ?
+                                            <li>
+                                                <a href={`https://www.twitter.com/${person.rrss.twitter}`} className="inline-flex items-center justify-center rounded-full text-xs h-6 w-6 bg-primary-light text-white shadow-lg shadow-primary/25" title="Ver Twitter" target="_blank">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="bi bi-twitter" viewBox="0 0 16 16"> <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/> </svg>
+                                                </a>
+                                            </li>
+                                            : null
+                                        }
+                                    </ul>
+                                    : null
+                                }
+                            </div>
+                            <div className="font-title mb-1 lg:mb-3 !leading-tight text-sm xl:text-base">{ person.name }</div>
+                            <div className="text-stone-400 font-serif text-sm leading-tight">{person.description}</div>
                         </div>
-                        <div className="font-title mb-1 lg:mb-3 !leading-tight text-sm xl:text-base">Erick Duarte</div>
-                        <div className="text-stone-400 font-serif text-sm leading-tight">Colaborador</div>
-                    </div>
-                    <div className="group bg-white rounded-2xl text-center p-2 md:p-4">
-                        <div className="mb-4">
-                            <img src={`${router.basePath}/assets/images/team/04.png`} alt="" className="aspect-square object-cover rounded-lg" />
-                        </div>
-                        <div className="font-title mb-1 lg:mb-3 !leading-tight text-sm xl:text-base">Jose Dionisio Morales</div>
-                        <div className="text-stone-400 font-serif text-sm leading-tight">Colaborador / Abogado</div>
-                    </div>
-                    <div className="group bg-white rounded-2xl text-center p-2 md:p-4">
-                        <div className="mb-4">
-                            <img src={`${router.basePath}/assets/images/team/06.png`} alt="" className="aspect-square object-cover rounded-lg" />
-                        </div>
-                        <div className="font-title mb-1 lg:mb-3 !leading-tight text-sm xl:text-base">Tirso Lecointere</div>
-                        <div className="text-stone-400 font-serif text-sm leading-tight">Colaborador / Programador web</div>
-                    </div>
-                    <div className="group bg-white rounded-2xl text-center p-2 md:p-4">
-                        <div className="mb-4">
-                            <img src={`${router.basePath}/assets/images/team/05.png`} alt="" className="aspect-square object-cover rounded-lg" />
-                        </div>
-                        <div className="font-title mb-1 lg:mb-3 !leading-tight text-sm xl:text-base">Lina Carpico</div>
-                        <div className="text-stone-400 font-serif text-sm leading-tight">Colaborador / Diseñadora</div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </section>
     </Main>)
 }
+
+const team = [
+    {
+        name: 'Fernando Gómez',
+        description: 'Coordinador / Guía Turístico',
+        rrss: {
+            instagram: 'fernandodropy'
+        },
+        photo: 'fernando.png'
+    },
+    {
+        name: 'José Santana',
+        description: 'Médico',
+        rrss: {
+            instagram: 'santanajd96'
+        },
+        photo: 'santana.png'
+    },
+    {
+        name: 'Alejandra Burgos',
+        description: 'Médico',
+        rrss: {
+            instagram: 'alejaburg'
+        },
+        photo: 'alejandra.png'
+    },
+    {
+        name: 'Carlos Torrealba',
+        description: 'Médico Veterinario',
+        photo: 'carlos.png'
+    },
+    {
+        name: 'Ling Hung',
+        description: 'Psicólogo',
+        rrss: {
+            instagram: 'lingcita'
+        },
+        photo: 'ling.png'
+    },
+    {
+        name: 'Juan Miguel Paolini',
+        description: 'Rescatista',
+        rrss: {
+            instagram: 'juanmihlf'
+        },
+        photo: 'juanmi.png'
+    },
+    {
+        name: '"Juancho" Nuñez',
+        description: 'Montañista / Trail Runner',
+        rrss: {
+            instagram: 'juanchonz'
+        },
+        photo: 'juancho.png'
+    },
+    {
+        name: 'José Dionisio Morales',
+        description: 'Abogado',
+        photo: 'josemorales.png'
+    },
+    {
+        name: 'Tirso Lecointere',
+        description: 'Fotógrafo / Programador web',
+        rrss: {
+            instagram: 'tirsolecointere'
+        },
+        photo: 'tirso.png'
+    },
+    {
+        name: 'Lina Carpico',
+        description: 'Diseñadora',
+        rrss: {
+            instagram: 'linacarpico'
+        },
+        photo: 'lina.png'
+    },
+    {
+        name: 'Andrea Rodriguez',
+        description: 'Médico',
+        rrss: {
+            instagram: 'deastef'
+        },
+        photo: 'andrea.png'
+    },
+    {
+        name: 'Erick Duarte',
+        description: 'Colaborador',
+        rrss: {
+            twitter: 'erick_algo'
+        },
+        photo: 'erick.png'
+    },
+];
 
 export default Index;
